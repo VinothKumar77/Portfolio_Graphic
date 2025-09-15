bf4992dba3ca6e02f3a253b1a5b6c164756824ab
vinoth kumar <100216800+VinothKumar77@users.noreply.github.com>
2025-09-15 22:43:54 +0530

    initial submit

---
+++ user.js
@@ -0,0 +1,88 @@
+import illustratorLogo from "../assets/Illustrator.png";
+import photoshopLogo from "../assets/Photoshop.png";
+import figmaLogo from "../assets/Figma.png";
+
+const INFO = {
+	main: {
+		title: "Reactfolio by Vinoth Kumar",
+		name: "Vinoth Kumar",
+		email: "vinothkumarl2410@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Graphic Designer nd UI/UX Designer",
+		description:
+			"A passionate Graphic Designer with a sharp eye for detail, specializing in UI/UX, branding, and creative visuals. Skilled in Adobe Illustrator, Photoshop, Figma, and Canva — I love turning ideas into powerful designs that inspire and connect.",
+	},
+
+	about: {
+		title: "I’m Vinoth Kumar. I live in India, where I design the future.",
+		description:
+			"I'm a Graphic Designer with a flair for bold and purposeful design. My expertise spans UI/UX design, logo creation, brochures, and digital illustrations. With freelancing and project experience, I’ve delivered impactful designs for startups, websites, and marketing campaigns. My goal is to craft visuals that not only look great but also tell a story and enhance user experiences.",
+	},
+
+	// articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	// },
+
+	projects: [
+		{
+			title: "Brochure - Water Treatment Company",
+			description:
+				"Corporate brochure design highlighting products & services.",
+			logo: illustratorLogo,
+			linkText: "View Project",
+			link: "https://drive.google.com/drive/folders/1Z07S7hXb7oPzwj-o6FxYl0NDk8iSr4M5?usp=sharing",
+		},
+
+		{
+			title: "Logo Design - Hardware Company",
+			description:
+				"Modern minimal logo for a hardware tools company, created in Adobe Illustrator.",
+			logo: illustratorLogo,
+			linkText: "View Project",
+			link: "https://drive.google.com/drive/folders/1Z07S7hXb7oPzwj-o6FxYl0NDk8iSr4M5?usp=sharing",
+		},
+
+		{
+			title: "Business Cards – Various Clients",
+			description:
+				"Print-ready business card designs in Canva and Illustrator.",
+			logo: illustratorLogo,
+			linkText: "View Project",
+			link: "https://drive.google.com/drive/folders/1Z07S7hXb7oPzwj-o6FxYl0NDk8iSr4M5?usp=sharing",
+		},
+
+		{
+			title: "UI/UX Design for E-Commerece Website-RP GROUPS",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: figmaLogo,
+			linkText: "View Project",
+			link: "https://drive.google.com/drive/folders/1Z07S7hXb7oPzwj-o6FxYl0NDk8iSr4M5?usp=sharing",
+		},
+
+		{
+			title: "UI/UX Design for E-Commerece Website-TRADTITONAL GROCERIES",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: figmaLogo,
+			linkText: "View Project",
+			link: "https://drive.google.com/drive/folders/1Z07S7hXb7oPzwj-o6FxYl0NDk8iSr4M5?usp=sharing",
+		},
+	],
+};
+
+export default INFO;
